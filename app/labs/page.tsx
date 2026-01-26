d6d9400458b14cb272afbf4ff161845f1fca3371
Baris Inandioglu <68742481+baris-inandi@users.noreply.github.com>
2026-01-25 21:25:06 -0500

    lab 1 complete

---
+++ app/labs/page.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+export default function Labs() {
+  return (
+    <div id="wd-labs">
+      <h1>Labs</h1>
+      <p>
+        <Link
+          href="https://github.com/baris-inandi/kambaz-next-js"
+          id="wd-github"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub Repository
+        </Link>
+      </p>
+      <ul>
+        <li>
+          <Link href="/labs/lab1" id="wd-lab1-link">
+            Lab 1: HTML Examples{" "}
+          </Link>
+        </li>
+        <li>
+          <Link href="/labs/lab2" id="wd-lab2-link">
+            Lab 2: CSS Basics{" "}
+          </Link>
+        </li>
+        <li>
+          <Link href="/labs/lab3" id="wd-lab3-link">
+            Lab 3: JavaScript Fundamentals{" "}
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
