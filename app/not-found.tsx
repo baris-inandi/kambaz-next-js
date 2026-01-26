a681665da563b0e7ad2c370f937214abae72928f
Baris Inandioglu <68742481+baris-inandi@users.noreply.github.com>
2026-01-25 22:45:59 -0500

    i use tailwind too much. i love tailwind.

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,15 +2,13 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div className="p-4">
-      <h2 className="text-danger">Page Not Found</h2>
+    <div>
+      <h2>Page Not Found</h2>
       <p>
         The requested page could not be found. Please check the page URL or
         return to the dashboard.
       </p>
-      <Link href="/dashboard" className="btn btn-primary mt-3">
-        Back to Dashboard
-      </Link>
+      <Link href="/dashboard">Back to Dashboard</Link>
     </div>
   );
 }
